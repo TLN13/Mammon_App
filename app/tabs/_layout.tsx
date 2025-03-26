580defad03dd55d224e9e1f2ebd0291002c38ce0
Cody Nguyen <Thanh.ng13@hotmail.com>
2025-03-26 16:09:16 -0600

    decorated nav bar

--- app/tabs/_layout.tsx
+++ app/tabs/_layout.tsx
@@ -1,24 +1,43 @@
 import { Tabs } from 'expo-router';
 import { MaterialIcons } from '@expo/vector-icons';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen
-        name="budget"
-        options={{
-          title: 'Budget',
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons name="account-balance-wallet" size={24} color={color} />
-          ),
-        }}
-      />
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: '#8BB04F',  
+        tabBarInactiveTintColor: '#ffffff',   
+        tabBarLabelStyle: {
+          fontSize: 13,                   
+          marginBottom: 10,                 
+        },
+        tabBarIconStyle: {
+          marginTop: 8,                    
+        },
+        tabBarStyle: {
+          backgroundColor: '#980058',      
+          height: 75,                      
+          paddingBottom: 10,               
+        },
+      }}
+    >
       <Tabs.Screen
         name="home"
         options={{
           title: 'Home',
           tabBarIcon: ({ color }) => (
-            <MaterialIcons name="home" size={24} color={color} />
+            <MaterialIcons name="home" size={32} color={color} />  // Bigger icon
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="budget"
+        options={{
+          title: 'Budget',
+          tabBarIcon: ({ color }) => (
+            <FontAwesome6 name="wallet" size={25} color={color} />  // Bigger icon
           ),
         }}
       />
@@ -27,7 +46,7 @@ export default function TabLayout() {
         options={{
           title: 'Overview',
           tabBarIcon: ({ color }) => (
-            <MaterialIcons name="pie-chart" size={24} color={color} />
+            <MaterialIcons name="pie-chart" size={30} color={color} />  // Bigger icon
           ),
         }}
       />
